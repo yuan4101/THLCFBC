b58fdbf178a697f7c1c8aafdb08aeb1f32107394
Juan Ante <juan09ante@gmail.com>
2022-02-26 22:04:16 -0500

    temp and hum personalized

--- THLCFBC.X/dht.c
+++ THLCFBC.X/dht.c
@@ -22,49 +22,28 @@ void DHT11Start()
 short DHT11CheckResponse()
 {
     short status = 0;
-    if(dataIn == 0)
-    {
+    if(dataIn == 0){
         __delay_us(80);
-        if(dataIn == 1)
-        {
+        if(dataIn == 1){
             __delay_us(80);
             status = 1;
-        } 
+        }
     }
     return status;
 }
 
-/*
-void DHT11_Start()
-{    
-    Data_Dir = 0;       // set as output port
-    Data_Out = 0;       // send low pulse of min. 18 ms width
-    __delay_ms(18);
-    Data_Out = 1;       // pull data bus high
-    __delay_us(40);
-    Data_Dir = 1;       // set as input port   
-}
-
-void DHT11_CheckResponse()
-{
-    while(Data_In & 1);     // wait till bus is High    
-    while(!(Data_In & 1));  // wait till bus is Low
-    while(Data_In & 1);     // wait till bus is High
-}
-
-char DHT11_ReadData()
+char DHT11ReadData()
 {
   char i,data = 0;  
     for(i=0;i<8;i++)
     {
-        while(!(Data_In & 1));      // wait till 0 pulse, this is start of data pulse
+        while(!(dataIn & 1));      /* wait till 0 pulse, this is start of data pulse */
         __delay_us(30);         
-        if(Data_In & 1)             // check whether data is 1 or 0   
+        if(dataIn & 1)             /* check whether data is 1 or 0 */    
           data = ((data<<1) | 1); 
         else
           data = (data<<1);  
-        while(Data_In & 1);
+        while(dataIn & 1);
     }
   return data;
 }
-*/
--- THLCFBC.X/main.c
+++ THLCFBC.X/main.c
@@ -7,114 +7,55 @@
 
 #include "configPICHeaderFile.h"
 #include "LCD16x2HeaderFile.h"
-//#include "DHTHeaderFile.h"
+#include "DHTHeaderFile.h"
 #include "functions.h"
 #include <stdio.h>
 #include <xc.h>
 #define _XTAL_FREQ 8000000
-#include "DHTLib.h"
 
 void main(void) {
-    uint8_t tb = 0, hb = 0;
-    char value[20];
+    char humidityInt, humidityDec, temperatureInt, temperatureDec, checksum, humidity[10], temperature[10];
+    
     OSCCON=0x72;                         // Use Internal Oscillator with Frequency 8MHZ
     ADCON1=0x0F;
-    LCD_Init();
-    dhtlib_init();
     
-    LCD_String_xy(0,0,"Starting...");
-    msDelay(2000);
-    LCD_Clear();
+    LCD_Init();
     
     while(1)
     {
-        enum dht_status result = dhtlib_read11(&tb, &hb);
-        if (result == E_DHTLIB_OK)
-			sprintf(value, "T: %02d C H: %02d%%  ", tb, hb);
-		else
-			sprintf(value, "Sensor error: %02d    ", result);
-        LCD_String_xy(0,0,value);
-        msDelay(5000);
         LCD_Clear();
         
-        //DHT11Start();
-        /*
+        DHT11Start();
+        
         if(DHT11CheckResponse())
         {
-            LCD_String_xy(0,0,"Success");
-            msDelay(1000);
-            LCD_Clear();
+            // read 40-bit data from DHT11 module
+            humidityInt = DHT11ReadData();          // read Relative Humidity's integral value
+            humidityDec = DHT11ReadData();          // read Relative Humidity's decimal value
+            temperatureInt = DHT11ReadData();       // read Temperature's integral value
+            temperatureDec = DHT11ReadData();       // read Relative Temperature's decimal value
+            checksum = DHT11ReadData();             // read 8-bit checksum value
+            
+            // convert humidity value to ascii and send it to display
+            //sprintf(buf, "T: %02d C H: %02d%%  ", tb, hb);
+            sprintf(humidity,"H: %02d%%",humidityInt);
+            LCD_String_xy(0,0,humidity);
+    
+            /* convert temperature value to ascii and send it to display*/
+            sprintf(temperature,"T: %02d C",temperatureInt);
+            LCD_String_xy(2,0,temperature);
+    
+            /* check addition of humidity and temperature value equals to checksum */
+            if(checksum != (humidityInt + humidityDec + temperatureInt + temperatureDec))
+                LCD_String_xy(0,8,"Error");
+            else
+                LCD_String_xy(0,8,"No Error");
         }
         else
         {
             LCD_String_xy(0,0,"No response!");
-            msDelay(1000);
-            LCD_Clear();
         }
-        */
         
-        /*
-        MSdelay(1000);
-        
-        LCD_String_xy(0,0,"Hola!");
-        MSdelay(500);
-    
-        DHT11_Start();
-        //DHT11_CheckResponse();          // wait for response from DHT11 module
-        
-        LCD_String_xy(0,0,"Start");
-        MSdelay(500);
-            
-        // read 40-bit data from DHT11 module
-        RH_Integral = DHT11_ReadData(); // read Relative Humidity's integral value
-        RH_Decimal = DHT11_ReadData();  // read Relative Humidity's decimal value
-        T_Integral = DHT11_ReadData();  // read Temperature's integral value
-        T_Decimal = DHT11_ReadData();   // read Relative Temperature's decimal value
-        Checksum = DHT11_ReadData();    // read 8-bit checksum value
-            
-        // convert temperature value to ascii and send it to display
-        sprintf(value,"%d",RH_Integral);
-        LCD_String_xy(0,0,value);
-        MSdelay(2000);
-        sprintf(value,".%d ",RH_Decimal);
-        LCD_String(value);
-        MSdelay(2000);
-        LCD_Char('%');
-        MSdelay(2000);
-            
-        // convert temperature value to ascii and send it to display
-        sprintf(value,"%d",T_Integral);
-        LCD_String_xy(1,0,value);
-        MSdelay(2000);
-        sprintf(value,".%d",T_Decimal);
-        LCD_String(value);
-        MSdelay(2000);
-        LCD_Char(0xdf);
-        MSdelay(2000);
-        LCD_Char('C');
-        MSdelay(2000);
-    
-        sprintf(value,"%d  ",Checksum);
-        LCD_String_xy(1,8,value);
-        MSdelay(2000);
-            
-        // check addition of humidity and temperature value equals to checksum
-        if(Checksum != (RH_Integral + RH_Decimal + T_Integral + T_Decimal))
-        {
-            LCD_String_xy(0,8,"Error");
-            MSdelay(2000);
-        }
-        else
-        {
-            LCD_String_xy(0,8,"No Error");
-            MSdelay(2000);
-        }
-    
-        MSdelay(500);
-        
-        LCD_String_xy(0,0,"Read");
-        MSdelay(1000);
-        LCD_Clear();
-        */
+        msDelay(1000);
     }
 }
